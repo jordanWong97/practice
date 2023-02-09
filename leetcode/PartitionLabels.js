220b0e61bc3f43c43b67322375d9798519029873
Jordan Wong <pjwong97@gmail.com>
2023-02-08 17:45:00 -0800

    permutations of arr of nums

--- leetcode/PartitionLabels.js
+++ leetcode/PartitionLabels.js
@@ -14,11 +14,17 @@ const partitionLabels = function (s) {
   const partitionLengths = [];
 
   for (let i = 0; i < s.length; i++) {
+    //if i is equal to last instance of s[i]
     if (i === last) {
+      //push difference from index - start + 1
       partitionLengths.push(i - start + 1);
       start = i + 1;
+      //last becomes the last index of next letter
       last = indexTracker[s[i + 1]];
     } else {
+      //this else takes care of next unique
+      //case for :ababcbacadefegdehijhklij returns [9,7,8]
+      //without else, would return [9,6,1,4,1,1,1]
       last = Math.max(last, indexTracker[s[i + 1]]);
     }
   }
